a02098ab4063b1a9a9c453e3cf654632387a2782
AL <168145100+ALprjkt@users.noreply.github.com>
2024-07-17 11:02:34 +0700

    Update ysl_build_script.sh

--- scripts/ysl_build_script.sh
+++ scripts/ysl_build_script.sh
@@ -9,7 +9,7 @@ clang() {
     rm -rf clang
     echo "Cloning clang"
     if [ ! -d "clang" ]; then
-        git clone https://gitlab.com/nekoprjkt/aosp-clang -b 17 --depth=1 clang
+        git clone https://github.com/kdrag0n/proton-clang -b master --depth=1 clang
         PATH="${PWD}/clang/bin:${PATH}"
     fi
     sudo apt install -y ccache
